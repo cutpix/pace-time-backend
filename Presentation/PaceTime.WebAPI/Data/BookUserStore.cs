03e44d3a886d3c3f4cf3e1867bfd23a75e9e44a9
Andrei Pavlyuk <devdice@outlook.com>
2017-09-03 01:00:11 +0300

    Create a couple of helpful abstractions

---
+++ Presentation/PaceTime.WebAPI/Data/BookUserStore.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaceTime.WebAPI.Data
+{
+    public class BookUserStore : UserStore<IdentityUser>
+    {
+
+    }
+}
---
+++ Presentation/PaceTime.WebAPI/Managers/BookUserManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PaceTime.WebAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaceTime.WebAPI.Managers
+{
+    public class BookUserManager : UserManager<IdentityUser>
+    {
+        public BookUserManager()
+            : base(new BookUserStore())
+        {
+        }
+    }
+}
