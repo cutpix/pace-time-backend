1b9fd1996bae77549a829423072ed1d8a10fe959
Andrei Pavlyuk <devdice@outlook.com>
2017-09-03 18:42:54 +0300

    added startup class for owin auth configuration

---
+++ Presentation/PaceTime.WebAPI/App_Start/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+
+namespace PaceTime.WebAPI
+{
+    public partial class Startup
+    {
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+        }
+    }
+}
---
+++ Presentation/PaceTime.WebAPI/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PaceTime.WebAPI.Startup))]
+
+namespace PaceTime.WebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureOAuth(app);
+        }
+    }
+}
