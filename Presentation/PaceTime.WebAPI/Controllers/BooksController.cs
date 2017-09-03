cf59848f953ae97eb7890f9c531949a9482c4d60
Andrei Pavlyuk <devdice@outlook.com>
2017-09-03 17:24:13 +0300

    Added books controller

--- Presentation/PaceTime.WebAPI/Controllers/BooksController.cs
+++ Presentation/PaceTime.WebAPI/Controllers/BooksController.cs
@@ -3,8 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
 
 namespace PaceTime.WebAPI.Controllers
